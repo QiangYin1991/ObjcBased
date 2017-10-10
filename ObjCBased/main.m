6836619126ab247e8aac7d5d634621619fa4168a
qiangyin <qiangyin1991@gmail.com>
2017-10-10 11:21:37 +0800

    将C++代码修改为OC代码

--- ObjCBased/main.m
+++ ObjCBased/main.m
@@ -51,57 +51,125 @@ NSString *colorName(ShapeColor color)
     return @"no Color";
 }
 
-void drawCircle(ShapeRect bounds, ShapeColor color)
-{
-    NSLog(@"draw a circle at ( %d %d %d %d ) in %@", bounds.x, bounds.y, bounds.width, bounds.height, colorName(color));
-}
-
-void drawRectangle(ShapeRect bounds, ShapeColor color)
-{
-    NSLog(@"draw a rectangle at ( %d %d %d %d ) in %@", bounds.x, bounds.y, bounds.width, bounds.height, colorName(color));
-}
-
-void drawOblateSpheroid(ShapeRect bounds, ShapeColor color)
-{
-    NSLog(@"draw a OblateSpheroid at ( %d %d %d %d ) in %@", bounds.x, bounds.y, bounds.width, bounds.height, colorName(color));
-}
-
-void drawShapes (Shape shapes[], int count)
+void drawShapes(id shapes[], int count)
 {
     for (int i = 0; i < count; ++i) {
-        switch (shapes[i].type) {
-            case kCircle:
-                drawCircle(shapes[i].bounds, shapes[i].fillColor);
-                break;
-            case kRectangle:
-                drawRectangle(shapes[i].bounds, shapes[i].fillColor);
-                break;
-            case kOblateSpheroid:
-                drawOblateSpheroid(shapes[i].bounds, shapes[i].fillColor);
-                break;
-            default:
-                break;
-        }
+        id shape = shapes[i];
+        [shape draw];
     }
 }
 
+// the interface of Class Circle
+@interface Circle: NSObject
+{
+    ShapeColor fillColor;
+    ShapeRect bounds;
+}
+
+- (void) setFillColor: (ShapeColor) color;
+- (void) setBounds: (ShapeRect) bs;
+- (void) draw;
+
+@end
+
+@implementation Circle
+
+- (void) setFillColor:(ShapeColor)color
+{
+    fillColor = color;
+}   // setFillColor
+
+- (void) setBounds:(ShapeRect)bs
+{
+    bounds = bs;
+}   // setBounds
+
+- (void) draw
+{
+    NSLog(@"draw a circle at ( %d %d %d %d ) in %@", bounds.x, bounds.y, bounds.width, bounds.height, colorName(fillColor));
+}
+@end    // circle
+
+// the interface of Class Rectangle
+@interface Rectangle: NSObject
+{
+    ShapeColor fillColor;
+    ShapeRect bounds;
+}
+
+- (void) setFillColor: (ShapeColor) color;
+- (void) setBounds: (ShapeRect) bs;
+- (void) draw;
+
+@end
+
+@implementation Rectangle
+
+- (void) setFillColor:(ShapeColor)color
+{
+    fillColor = color;
+}   // setFillColor
+
+- (void) setBounds:(ShapeRect)bs
+{
+    bounds = bs;
+}   // setBounds
+
+- (void) draw
+{
+    NSLog(@"draw a Rectangle at ( %d %d %d %d ) in %@", bounds.x, bounds.y, bounds.width, bounds.height, colorName(fillColor));
+}
+@end    // Rectangle
+
+// the interface of Class Circle
+@interface OlateSpheroid: NSObject
+{
+    ShapeColor fillColor;
+    ShapeRect bounds;
+}
+
+- (void) setFillColor: (ShapeColor) color;
+- (void) setBounds: (ShapeRect) bs;
+- (void) draw;
+
+@end
+
+@implementation OlateSpheroid
+
+- (void) setFillColor:(ShapeColor)color
+{
+    fillColor = color;
+}   // setFillColor
+
+- (void) setBounds:(ShapeRect)bs
+{
+    bounds = bs;
+}   // setBounds
+
+- (void) draw
+{
+    NSLog(@"draw a circle at ( %d %d %d %d ) in %@", bounds.x, bounds.y, bounds.width, bounds.height, colorName(fillColor));
+}
+@end    // OlateSpheroid
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Shape shapes[3];
+        id shapes[3];
         ShapeRect rect0 = {0, 0, 10, 30};
-        shapes[0].type = kCircle;
-        shapes[0].fillColor = kRedColor;
-        shapes[0].bounds = rect0;
-        
+        shapes[0] = [Circle new];
+        [shapes[0] setFillColor:kRedColor];
+        [shapes[0] setBounds:rect0];
+
         ShapeRect rect1 = {10, 30, 20, 40};
-        shapes[1].type = kRectangle;
-        shapes[1].fillColor = kGreenColor;
-        shapes[1].bounds = rect1;
+        shapes[1] = [Rectangle new];
+        [shapes[1] setFillColor:kGreenColor];
+        [shapes[1] setBounds:rect1];
         
         ShapeRect rect2 = {20, 40, 30 ,50};
-        shapes[2].type = kOblateSpheroid;
-        shapes[2].fillColor = kBlueColor;
-        shapes[2].bounds = rect2;
+        shapes[2] = [OlateSpheroid new];
+        [shapes[2] setFillColor:kBlueColor];
+        [shapes[2] setBounds:rect2];
         
         drawShapes(shapes, 3);
     }
