6d1de948df5a2358d059019204dc5f35347cbc6f
qiangyin <qiangyin1991@gmail.com>
2017-10-10 08:30:07 +0800

    Initial Commit

---
+++ ObjCBased/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjCBased
+//
+//  Created by sohu on 2017/10/10.
+//  Copyright © 2017年 sohu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
